2f8ae4bac92a98279f33a6be954c3fc0aa2301e3
Abhishek Sridhar <abhisridhar9@gmail.com>
2021-09-21 11:15:11 -0500

    final report

--- Lab3_final/problem2.py
+++ Lab3_final/problem2.py
@@ -21,7 +21,6 @@ for link in soup.select("a[href$='.pdf']"):
     #Name the pdf files using the last portion of each link which are unique in this case
     filename = os.path.join(folder_location,link['href'].split('/')[-1])
     with open(filename, 'wb') as f:
-        print(f)
         f.write(requests.get(urljoin(url,link['href'])).content)
 
 
